7e494ba15843a1bf36e6957ec846406a84dbb3ef
SANGHYUN LEE <norimsu@gmail.com>
2020-10-24 00:16:04 +0900

    CH02.01-Start-BankStatementAnalyzer

---
+++ src/main/java/bsa/BankStatementAnalyzerSimple.java
@@ -0,0 +1,43 @@
+package bsa;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class BankStatementAnalyzerSimple {
+
+	private static final String RESOURCES = "src/main/resources/";
+
+	private BankStatementAnalyzerSimple() {
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		final Path path = Paths.get(RESOURCES + "bank-data-simple.csv");
+		final List<String> lines = Files.readAllLines(path);
+		double total = 0d;
+		for (final String line : lines) {
+			final String[] columns = line.split(",");
+			final double amount = Double.parseDouble(columns[1]);
+			total += amount;
+		}
+		System.out.println("The total for all transactions is " + total);
+
+		total = 0;
+		final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		for (final String line : lines) {
+			final String[] columns = line.split(",");
+			final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+			if (date.getMonth() == Month.JANUARY) {
+				final double amount = Double.parseDouble(columns[1]);
+				total += amount;
+			}
+		}
+		System.out.println("The total for all transactions in January is " + total);
+	}
+}
