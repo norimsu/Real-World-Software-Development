4492651f4348f7c6e1d57f2a419f6e62338e327c
SANGHYUN LEE <norimsu@gmail.com>
2020-10-24 03:19:22 +0900

    CH03.01-Start-BankStatementAnalyzer

--- src/main/java/bsa/BankStatementProcessor.java
+++ src/main/java/bsa/BankStatementProcessor.java
@@ -1,6 +1,7 @@
 package bsa;
 
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BankStatementProcessor {
@@ -37,5 +38,54 @@ public class BankStatementProcessor {
 			}
 		}
 		return total;
+	}
+
+	/**
+	 * 특정_금액 이상의 은행거래내역 찾기
+	 *
+	 * @param amount 특정_금액
+	 * @return 은행거래내역
+	 */
+	public List<BankTransaction> findTransactionsGreaterThanEqual(final int amount) {
+		final ArrayList<BankTransaction> result = new ArrayList<>();
+		for (BankTransaction bankTransaction : bankTransactions) {
+			if (bankTransaction.getAmount() >= amount) {
+				result.add(bankTransaction);
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * 특정_월의 은행거래내역 찾기
+	 *
+	 * @param month 특정_월
+	 * @return 은행거래내역
+	 */
+	public List<BankTransaction> findTransactionsInMonth(final Month month) {
+		final ArrayList<BankTransaction> result = new ArrayList<>();
+		for (BankTransaction bankTransaction : bankTransactions) {
+			if (bankTransaction.getDate().getMonth() == month) {
+				result.add(bankTransaction);
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * 특정_월 이나 특정_금액 으로 은행거래내역 찾기
+	 *
+	 * @param month  특정_월
+	 * @param amount 특정_금액
+	 * @return 은행거래내역
+	 */
+	public List<BankTransaction> findTransactionsInMonthAndGreater(final Month month, final int amount) {
+		final ArrayList<BankTransaction> result = new ArrayList<>();
+		for (BankTransaction bankTransaction : bankTransactions) {
+			if (bankTransaction.getDate().getMonth() == month && bankTransaction.getAmount() >= amount) {
+				result.add(bankTransaction);
+			}
+		}
+		return result;
 	}
 }
